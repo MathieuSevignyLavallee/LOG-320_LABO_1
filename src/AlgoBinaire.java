697678cb0e8f723864033e01582e41760367f4d5
MathieuSevignyLavallee <89943988+MathieuSevignyLavallee@users.noreply.github.com>
2022-05-31 17:03:20 -0400

    up

--- src/AlgoBinaire.java
+++ src/AlgoBinaire.java
@@ -2,27 +2,19 @@ package src;
 
 // 2 sections tableau
 public class AlgoBinaire {
-
     public int algo(int[] tab, int n, int val){
-        if(n == 1){
+        return helper(tab,0, tab.length, val);
+    }
+
+    public int helper(int[] tab, int low, int high, int val){
+        if(tab.length == 1){
             if (tab[0] == val){
                 return 0;
             }else{
                 return -1;
             }
         }
-        int mid = n/2;
 
-        if(tab[mid] == val){
-            return mid;
-        }
-        if(tab[mid] > val){
-            return new AlgoBinaire().helper(tab,0, mid-1, val);
-        }
-        return new AlgoBinaire().helper(tab,mid+1, n, val);
-    }
-
-    public int helper(int[] tab, int low, int high, int val){
         int mid = low + (high - low) / 2;
 
         if(tab[mid] == val){

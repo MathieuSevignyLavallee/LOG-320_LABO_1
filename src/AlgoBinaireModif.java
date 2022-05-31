06487cb3034ee95a95a7cd1c829241f747afb3e7
MathieuSevignyLavallee <89943988+MathieuSevignyLavallee@users.noreply.github.com>
2022-05-31 17:02:28 -0400

    update binaire

--- src/AlgoBinaireModif.java
+++ src/AlgoBinaireModif.java
@@ -3,6 +3,26 @@ package src;
 // 3 sections tableau
 public class AlgoBinaireModif {
     public int algo(int[] tab, int n, int val){
-       return -1;
+        return helper(tab,0, tab.length, val);
+    }
+
+    public int helper(int[] tab, int low, int high, int val){
+        if(tab.length == 1){
+            if (tab[0] == val){
+                return 0;
+            }else{
+                return -1;
+            }
+        }
+
+        int mid = low + (high - low) / 2;
+
+        if(tab[mid] == val){
+            return mid;
+        }
+        if(tab[mid] > val){
+            return new AlgoBinaire().helper(tab,0, mid-1, val);
+        }
+        return new AlgoBinaire().helper(tab,mid+1, high, val);
     }
 }
